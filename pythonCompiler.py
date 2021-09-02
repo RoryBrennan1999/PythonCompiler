aaebf9d40b6bed05081b81e69d6ba8bb5966fba6
RoryBrennan1999 <rorybrennan2020@gmail.com>
2021-09-02 14:11:50 +0100

    Initial Commit

---
+++ pythonCompiler.py
@@ -0,0 +1,10 @@
+############################################################
+# Code Generator for CPL Python Compiler                   #
+# Takes AST from pythonParser.py and generates             #
+# machine code using llvmlite                              #
+# Written by Rory Brennan [18237606]                       #
+# 02/09/2021                                               #
+############################################################
+
+from llvmlite import ir
+
